8a0926b0fe9f4fe329ed9f538443d7cae7075504
talitavitoriapereira <159267033+talitavitoriapereira@users.noreply.github.com>
2024-06-17 16:36:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// function bomdia (){
+//     nome = prompt('digite seu nome:');
+//     alert(nome)
+// }
+
+function troca_item(){
+    var texto1= document.getElementById('texto1');
+    var imagem1=document.getElementById('img1');
+
+    if(imagem1agem1.scr.match("img/h1")){
+        imagem1.scr="img/h1,5";
+    } else{
+        imagem1.scr="img/h1";
+    }
+
+    if(texto1.innerHTML==="Azul"){
+        texto1.innerHTML="Rosa"
+    } else{
+        texto1.innerHTML="Azul"
+    }
+}
